528b2729de4d9bec9b1a2f79b08ffa1ee3b03a41
sandy <sandydwi75@gmail.com>
2025-05-10 16:56:46 +0700

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- eslint: {
-  ignoreDuringBuilds: true
- }
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
